7602309a48ea78aeca062d3e3c5b2a667f83dc4b
Olaitan1 <holaitan4@gmail.com>
2022-12-14 14:47:40 +0100

    worked on conflicted files

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import SignUpForm from "./components/signUp/signUp";
 // import Footer from "./components/Footer/Footer";
-// import Home from "./pages/Home/Home";
+
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NavBar from "./components/navBar/navBar";
+// import "./App.css";
+
+import ResetPassword from "./pages/Home/ResetPassword/resetPassword";
 
 function App() {
 	return (
@@ -16,6 +19,7 @@ function App() {
 
 					<Route path="/navbar" element={<NavBar />} />
 					<Route path="/sign-up" element={<SignUpForm />} />
+					<Route path="/reset-password" element={<ResetPassword />} />
 				</Routes>
 			</Router>
 		</React.Fragment>
