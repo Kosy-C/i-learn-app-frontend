843b0546605634aa372021e9de904c7d5307d034
Daniel Iwegbue <ifyd47@gmail.com>
2022-12-16 22:07:54 +0100

    add axious get request

--- src/components/studentHistoryPage/studentHistoryPage.tsx
+++ src/components/studentHistoryPage/studentHistoryPage.tsx
@@ -1,37 +1,30 @@
 import React,{useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import './student.css'
-import chem from '../../assets/chem.jpg'
 import maths from '../../assets/maths.jpg'
 import axios from 'axios'
-
-
-
+import { apiGet } from '../../utils/api/axios'
 const studentHistoryPage = () => {
-    const params = useParams<{id: string}>()
     const [courses, setCourses] = useState<any>([])
         const getHistory = async () => {
             const id = localStorage.getItem('id')
-            const {data} = await axios.get(`http://localhost:5000/student/history/${id}`)
+            const {data} = await apiGet(`/courses/getStudentHistory`)
             setCourses(data.courses)
         }  
         useEffect(() => {
             getHistory()   
-    }, [params.id])
-    
+    },[])
   return (
     <>
     {courses.map((course:any) => {
-
-      
 return <div className='container'>
             <div className='header'>
                 <h2>My Courses</h2>
             </div>
             <div className='card-container'>
                 <div className='card'>
-                    <img src={chem} alt="" className='img_container' />
-                    <div className='card-details'> 
+                    <img src={maths} alt="" className='img_container' />
+                    <div className='card-details'>
                         <div className='subj'>
                             <h3> <b>{course.title}<br/> {course.subtitle}</b></h3>
                             {/* <h3> <b>Chemistry for beginners:<br/>30 days perfection</b></h3> */}
@@ -43,20 +36,12 @@ return <div className='container'>
                             <p className='progressbar'></p>
                             <p>Your progress</p>
                         </div>
-                        
                     </div>
                 </div>
-                
             </div>
-
-            
         </div>
     })} 
-    
-
     </>
-    
   )
 }
-
 export default studentHistoryPage
--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -1,1 +1,11 @@
 
+import axios from "axios";
+const baseUrl = "http://localhost:4000";
+export const apiGet = (path: string) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("signature")}`,
+        },
+    };
+    return axios.get(`${baseUrl}${path}`, config);
+};
