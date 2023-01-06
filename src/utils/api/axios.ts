57d8123465c8640a206e2a1f08f3d98079fe9c51
zeen-at <sannizinat@gmail.com>
2023-01-06 16:18:28 +0100

    made some changes

--- src/utils/api/axios.ts
+++ src/utils/api/axios.ts
@@ -10,3 +10,40 @@ export const apiGet = async (path: string) => {
 	const result = await axios.get(`${baseUrl}${path}`, config);
 	return result;
 };
+
+// export const apiGet = (path: string, auth=true) => {
+//     const config = {
+//         headers: {
+//             Authorization: `Bearer ${localStorage.getItem("signature")}`,
+//         },
+//     };
+//     return axios.get(`${baseUrl}${path}`, config);
+// };
+export const apiPost = (path: string, conf={}) => {
+	console.log("conf is ", conf)
+    const config = {
+        ...conf,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("signature")}`,
+        },
+    };
+	console.log("config is ", config)
+    return axios.post(`${baseUrl}${path}`, conf);
+};
+export const apiUpdate = (path: string, conf={}, auth=true) => {
+    const config = {
+        ...conf,
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("signature")}`,
+        },
+    };
+    return axios.put(`${baseUrl}${path}`, config);
+};
+export const apiDelete = (path: string, auth=true) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem("signature")}`,
+        },
+    };
+    return axios.delete(`${baseUrl}${path}`, config);
+};
