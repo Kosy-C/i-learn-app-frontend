8228124651b3ba64dbaf9372ef7dfca35ebb6677
Deca <deca@GB.local>
2023-01-07 10:46:18 +0100

    all tutor first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import StudentHistoryPage from "./components/studentHistoryPage/studentHistoryPage";
-
 import SignUpForm from "./components/signUp/signUp";
 import LoginForm from "./components/Login/Login";
 // import Footer from "./components/Footer/Footer";
-
 import LandingPage from "./pages/LandingPage/LandingPage";
 import NavBar from "./components/navBar/navBar";
 import Contact from "./pages/Contact/Contact";
-
 import ResetPassword from "./pages/ResetPassword/resetPassword";
 import SetNewPassword from "./pages/ResetPassword/setNewPassword";
+import AllTutor from "./components/AllTutorComponent/AllTutor";
 
 function App() {
 	return (
@@ -30,6 +28,8 @@ function App() {
 					<Route path="/users/resetpassword" element={<SetNewPassword />} />
 
 					<Route path="/history-page" element={<StudentHistoryPage />} />
+					<Route path="/all-tutors" element={<AllTutor />} />
+					
 				</Routes>
 			</Router>
 		</React.Fragment>
