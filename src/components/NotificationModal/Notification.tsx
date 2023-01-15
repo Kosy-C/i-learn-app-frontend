f04fdbaa68ca5f90bcb30fd8d81674ce356b2ed9
Oluwaseyi <oluwaseyi.makinde@decagon.dev>
2023-01-15 20:37:19 +0100

    removed the pay button

--- src/components/NotificationModal/Notification.tsx
+++ src/components/NotificationModal/Notification.tsx
@@ -1,75 +1,93 @@
 import React, { useEffect, useState } from "react";
-// import notImage from "../../assets/images/notImage.svg";
 import Card from "../CardModal/Card";
 import "./Notification.css";
+import { useParams } from "react-router-dom";
 import { posts } from "./dataPosts";
-// import { Direction } from "react-toastify/dist/utils";
+import { apiGet } from "../../utils/api/axios";
 
-// interface NotificationM {
-//   name: string;
-//   message: string;
-//   time: string;
-// }
+interface NotificationM {
+  name: string;
+  description: string;
+  time: string;
+  status: string;
+  image: string;
+}
 
 const Notification: React.FC = () => {
-	// const [notifications, setNotifications] = useState<Notification[]>([]);
-	// const [loading, setLoading] = useState(false);
-	// const [error, setError] = useState("");
+  const [notifications, setNotifications] = useState<NotificationM[]>([]);
+  const [pay, setPay] = useState("pay");
+  const [payment, setPayment] = useState(true);
+  const [notifs, setNotifs] = useState(posts);
+  const params = useParams();
 
-	// useEffect(() => {
-	//   const fetchData = async () => {
-	//     setLoading(true);
-	//     try {
-	//       const response = await fetch("/api/notifications");
-	//       const data = await response.json();
-	//       setNotifications(data);
-	//     } catch (err) {
-	//       setError(err.message);
-	//     } finally {
-	//       setLoading(false);
-	//     }
-	//   };
+  const handleClick = (index: number, post: any) => {
+    // setPay((index) => {
+    //   if (index) {
+    //     return "confirmed";
+    //   }
+    // });
+    post.payment = "confirm";
+    const newNotif = notifs;
+    //console.log(notifs[index]);
+    // const allPost = [...notifs.filter((notif, ind) => ind !== index), post];
+    // console.log(post);
+    setNotifs(newNotif);
+    console.log(notifs);
+    // if (index) {
+    // 	setPayment(!payment);
+    // }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiGet(`/users/notifications/${params.id}`);
+        const data = response.data;
+        console.log(data);
+        setNotifications(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-	//   fetchData();
-	// }, []);
+    fetchData();
+  }, []);
 
-	// if (loading) {
-	//   return <p>Loading notifications...</p>;
-	// }
-
-	// if (error) {
-	//   return <p>Error: {error}</p>;
-	// }
-	return (
-		<div>
-			<Card>
-				{posts.slice(0, 5).map((post: any, index) => {
-					return (
-						<>
-							<div
-								key={index}
-								className="notification-user"
-								style={
-									post.status === "unread"
-										? { backgroundColor: "rgba(20, 168, 0, 0.05)" }
-										: { backgroundColor: "#ffffff" }
-								}
-							>
-								<img src={post.image} alt="userImage" />
-								<div className="notification-profile">
-									<h1>{post.name}</h1>
-									<p>{post.time}</p>
-									<div className="notification-message">
-										<p>{post.message}</p>
-									</div>
-								</div>
-							</div>
-							<hr className="notification-line" />
-						</>
-					);
-				})}
-			</Card>
-		</div>
-	);
+  return (
+    <div>
+      <Card>
+        {notifs.slice(0, 5).map((post: any, index) => {
+          return (
+            <>
+              <div
+                key={index}
+                className="notification-user"
+                style={
+                  post.status === "unread"
+                    ? { backgroundColor: "rgba(20, 168, 0, 0.05)" }
+                    : { backgroundColor: "#ffffff" }
+                }
+              >
+                <img src={post.image} alt="userImage" />
+                <div className="notification-profile">
+                  <h1>{post.name}</h1>
+                  <p>{post.time}</p>
+                  <div className="notification-message">
+                    <p>{post.message}</p>
+                  </div>
+                </div>
+                <button
+                  className="notification-button"
+                  onClick={() => handleClick(index, post)}
+                >
+                  {post.payment === "pending" ? "pay" : "confirm"}
+                </button>
+              </div>
+              <hr className="notification-line" />
+            </>
+          );
+        })}
+      </Card>
+    </div>
+  );
 };
 export default Notification;
