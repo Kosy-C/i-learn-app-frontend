5e49b21d64462fe45f2effaa24332af9205d3ba3
Kosy-C <kosychinweuba@gmail.com>
2023-01-17 15:59:53 +0100

    ilearn dasboard modified

--- src/components/profileDetails/profile.tsx
+++ src/components/profileDetails/profile.tsx
@@ -10,78 +10,78 @@ import { Modal } from "react-responsive-modal";
 import { Tutor } from "../../utils/Interfaces/index.dto";
 
 const Profile = () => {
-  const [tutor, setTutor] = useState<Tutor>();
-  const [modalIsOpen, setIsOpen] = useState(false);
+	const [tutor, setTutor] = useState<Tutor>();
+	const [modalIsOpen, setIsOpen] = useState(false);
 
-  function openModal() {
-    setIsOpen(true);
-  }
-  function closeModal() {
-    setIsOpen(false);
-  }
-  const params = useParams();
-  useEffect(() => {
-    const fetch = async () => {
-      const { data } = await apiGet(`/users/atutordetail/${params.id}`);
-      console.log(data);
-      setTutor(data.message);
-    };
-    fetch();
-  }, []);
-  return (
-    <>
-      <body className="profile-body">
-        <div className="profile-container">
-          <div className="profile-head">
-            <h2>Tutor Profile</h2>
-            <h2>
-              <button type="submit"></button>
-            </h2>
-          </div>
-          <hr />
-          <div className="profile-tutor">
-            <img src={tutor?.image} alt="avatar" />
-            <div className="profile-tutor-details">
-              <h2>{tutor?.name}</h2>
-              <p>
-                {" "}
-                <AiOutlineSafetyCertificate className="certify-icon" />{" "}
-                Certified Tutor
-              </p>
-              <p>
-                {" "}
-                <CiLocationOn /> {tutor?.email}
-              </p>
-            </div>
-          </div>
-          <div className="profile-about">
-            <h3>About</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
-              quia id eum doloribus, unde aliquam. Magni, ratione. Magni vero
-              animi id atque assumenda laudantium deserunt quam quisquam nam.
-              Nostrum, culpa?
-            </p>
-          </div>
-          <div className="profile-expertise">
-            <h3>Expertise</h3>
-            <div>
-              <ul className="profile-expertise-list">
-                <li>{tutor?.areaOfInterest}</li>
-              </ul>
-            </div>
-          </div>
-          <div className="availabilityButton">
-            <button type="submit" onClick={openModal}>
-              Availability
-            </button>
-            <Modal open={modalIsOpen} onClose={closeModal}>
-              <TutorAvailability />
-            </Modal>
-          </div>
-        </div>
-      </body>
-    </>
-  );
+	function openModal() {
+		setIsOpen(true);
+	}
+	function closeModal() {
+		setIsOpen(false);
+	}
+	const params = useParams();
+	useEffect(() => {
+		const fetch = async () => {
+			const { data } = await apiGet(`/users/atutordetail/${params.id}`);
+			console.log(data);
+			setTutor(data.message);
+		};
+		fetch();
+	}, []);
+	return (
+		<>
+			<body className="profile-body">
+				<div className="profile-container">
+					<div className="profile-head">
+						<h2>Tutor Profile</h2>
+						<h2>
+							<button type="submit"></button>
+						</h2>
+					</div>
+					<hr />
+					<div className="profile-tutor">
+						<img src={tutor?.image} alt="avatar" />
+						<div className="profile-tutor-details">
+							<h2>{tutor?.name}</h2>
+							<p>
+								{" "}
+								<AiOutlineSafetyCertificate className="certify-icon" />{" "}
+								Certified Tutor
+							</p>
+							<p>
+								{" "}
+								<CiLocationOn /> {tutor?.email}
+							</p>
+						</div>
+					</div>
+					<div className="profile-about">
+						<h3>About</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat
+							quia id eum doloribus, unde aliquam. Magni, ratione. Magni vero
+							animi id atque assumenda laudantium deserunt quam quisquam nam.
+							Nostrum, culpa?
+						</p>
+					</div>
+					<div className="profile-expertise">
+						<h3>Expertise</h3>
+						<div>
+							<ul className="profile-expertise-list">
+								<li>{tutor?.areaOfInterest}</li>
+							</ul>
+						</div>
+					</div>
+					<div className="availabilityButton">
+						<button type="submit" onClick={openModal}>
+							Availability
+						</button>
+						<Modal open={modalIsOpen} onClose={closeModal}>
+							<TutorAvailability />
+						</Modal>
+					</div>
+				</div>
+			</body>
+		</>
+	);
 };
 export default Profile;
