da09d656394b594da9499b9d1458e47b9e93dc5a
Erhimuebru <erhimu.ebru@decagon.dev>
2023-01-27 01:31:52 +0100

    changes

--- src/components/ProgressBar/ProgressBars.tsx
+++ src/components/ProgressBar/ProgressBars.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { apiGet } from '../../utils/api/axios';
+import { useState, useEffect } from 'react';
 import './ProgressBar.css'
 
 const ProgressBar = (props:any) => {
   const [width, setWidth] = useState(0);
   const [display, setDisplay] = useState("none");
-  const [courses, setCourses] = useState<any>([]);
-
+  
 
   useEffect(() => {
     if (props.myProp > 0) {
