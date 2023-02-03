8e203374297ecab65899d0e937aa835130324b59
22Theresecodes <105355739+22Theresecodes@users.noreply.github.com>
2023-02-03 19:52:44 +0100

    Button disabled

--- src/useContext/index.tsx
+++ src/useContext/index.tsx
@@ -4,93 +4,88 @@ import { apiGet, apiPost } from "../utils/api/axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
-  courseDetails,
-  CourseDetails,
+	courseDetails,
+	CourseDetails,
 } from "../pages/TutorCourseOperations/TutorCourseOperations";
 import { User } from "../utils/Interfaces/index.dto";
 import { AxiosResponse } from "axios";
 
-
 export interface LoginData {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 export interface GlobalStateInterface {
-  LoginConfig: (data: LoginData) => Promise<void>;
-  user: User | undefined;
-  loading: Boolean;
-  error: null | String;
-  loggedInUser: () => void;
-	setLoading: React.Dispatch<React.SetStateAction<Boolean>> |any;
+	LoginConfig: (data: LoginData) => Promise<void>;
+	user: User | undefined;
+	loading: Boolean;
+	error: null | String;
+	loggedInUser: () => void;
+	setLoading: React.Dispatch<React.SetStateAction<Boolean>> | any;
 }
 export const dataContext = createContext<GlobalStateInterface | null>(null);
 
 const DataProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<User>();
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<String | null>(null);
-  /** ==============Login======= **/
-  const LoginConfig: (data: LoginData) => Promise<void> = async (
-    data: LoginData
-  ) => {
-    try {
-      const response: AxiosResponse<any, any> = await apiPost(
-        "/users/login",
-        data
-      );
-      const signature: string = response.data.signature;
+	const [user, setUser] = useState<User>();
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<String | null>(null);
+	/** ==============Login======= **/
+	const LoginConfig: (data: LoginData) => Promise<void> = async (
+		data: LoginData
+	) => {
+		try {
+			const response: AxiosResponse<any, any> = await apiPost(
+				"/users/login",
+				data
+			);
+			const signature: string = response.data.signature;
 
-      localStorage.setItem("signature", signature);
-      localStorage.setItem("user", response.data.areaOfInterest || "backend");
-      localStorage.setItem("userType", response.data.userType);
-      if (response.status === 200) {
-        setLoading(false);
-        window.location.href = "/dashboard";
-      }
-    } catch (err: any) {
-      setLoading(false);
-      if (err.response?.data?.Error === "Internal server Error"){
-        toast.error("Something went wrong, please hang on");
-      } else {
-        toast.error(err.response?.data?.Error || "Something went wrong");
-      }
-    }
-  };
+			localStorage.setItem("signature", signature);
+			localStorage.setItem("user", response.data.areaOfInterest || "backend");
+			localStorage.setItem("userType", response.data.userType);
+			if (response.status === 200) {
+				setLoading(false);
+				window.location.href = "/dashboard";
+			}
+		} catch (err: any) {
+			setLoading(false);
+			if (err.response?.data?.Error === "Internal server Error") {
+				toast.error("Something went wrong, please hang on");
+			} else {
+				toast.error(err.response?.data?.Error || "Something went wrong");
+			}
+		}
+	};
 
-  // method should be placed into actions
-  const loggedInUser = async () => {
-    const { data } = await apiGet("/users/profile");
-    setUser(data.userDetails);
-    setLoading(false);
-  };
+	// method should be placed into actions
+	const loggedInUser = async () => {
+		const { data } = await apiGet("/users/profile");
+		setUser(data.userDetails);
+		setLoading(false);
+	};
 
-  return (
-    <dataContext.Provider
-      value={{
-        LoginConfig,
-        loggedInUser,
-        setLoading,
-        user,
-        loading,
-        error,
-      }}
-    >
-      {children}
-    </dataContext.Provider>
-  );
+	return (
+		<dataContext.Provider
+			value={{
+				LoginConfig,
+				loggedInUser,
+				setLoading,
+				user,
+				loading,
+				error,
+			}}
+		>
+			{children}
+		</dataContext.Provider>
+	);
 	// const [loading, setLoading] = useState<Boolean>(true);
-
-
-
-
 };
 
 export const useAuth = () => {
-  const context = React.useContext(dataContext) as GlobalStateInterface;
-  if (context === undefined) {
-    throw new Error("useAuth must be used within the auth provider");
-  }
-  return context;
+	const context = React.useContext(dataContext) as GlobalStateInterface;
+	if (context === undefined) {
+		throw new Error("useAuth must be used within the auth provider");
+	}
+	return context;
 };
 
 export default DataProvider;
