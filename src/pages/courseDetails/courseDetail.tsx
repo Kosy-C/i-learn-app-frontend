4625cca50511ceb942134a19eac9ad460c1419e9
ifiokInyang <ifiok.inyang@decagon.dev>
2023-02-07 11:39:33 +0100

    merged staging

--- src/pages/courseDetails/courseDetail.tsx
+++ src/pages/courseDetails/courseDetail.tsx
@@ -17,8 +17,9 @@ interface Rating {
 const CourseDetail = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [course, setCourse] = useState<CourseModel>();
+  const [ratings, setRatings] = useState<Rating[]>([]);
 
-  const { user, loggedInUser, loading } = useAuth();
+	const { user, loggedInUser, loading } = useAuth();
 
   // const [email, setEmail] = useState(user?.email);
   const params = useParams();
@@ -27,27 +28,28 @@ const CourseDetail = () => {
       const { data } = await apiGet(`/courses/get-course/${params.id}`);
       setCourse(data.course);
       course?.rating;
+      setRatings(data.course.course_ratings);
     };
     getCourseDetail();
     loggedInUser();
   }, []);
 
-  const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1);
-  };
+	const navigate = useNavigate();
+	const goBack = () => {
+		navigate(-1);
+	};
 
-  const makePayment = () => {
-    setModalOpen(true);
-  };
+	const makePayment = () => {
+		setModalOpen(true);
+	};
 
-  return user == null || course == null ? (
-    <LoaderRings />
-  ) : (
-    <>
-      <button className="cd-rate_course_arrowButton" onClick={goBack}>
-        &#8249; Go Back
-      </button>
+	return user == null || course == null ? (
+		<LoaderRings />
+	) : (
+		<>
+			<button className="cd-rate_course_arrowButton" onClick={goBack}>
+				&#8249; Go Back
+			</button>
 
       <div className="heading-border">
         <h1 className="cd-heading"> About the course</h1>
@@ -57,61 +59,68 @@ const CourseDetail = () => {
           className="cd-course-container"
           style={{
             backgroundImage: `url(
-            ${course.course_image}
-          )`,
+              ${course.course_image}
+              )`,
           }}
         >
-          <h2 className="cd-title">{course.title}</h2>
-          <p className="cd-p">{course.description}</p>
-          <div className="cd-rating">
-            <Rating rating={Number(course.rating)} image={""} color={""} />
-          </div>
-          <p>
-            Updated{" "}
-            {course && new Date(course.createdAt).toLocaleString("en-NG")}
-          </p>
+          <div className="course-overlay">
+            <div className="course--container__content">
+              <h2 className="cd-title">{course.title}</h2>
+              <p className="cd-p">{course.description}</p>
+              <div className="cd-rating">
+                <Rating rating={course.rating} image={""} color={""} />
+              </div>
+              <p>
+                Updated{" "}
+                {course && new Date(course.createdAt).toLocaleString("en-NG")}
+              </p>
 
-          <p>{`₦${Number(course.pricing).toLocaleString()}`}</p>
+              <h4>{`₦${Number(course.pricing).toLocaleString()}`}</h4>
+            </div>
+          </div>
         </div>
 
         <div className="cd-tutor-container">
-          <p className="cd-tutor-title">About the Tutor</p>
+          <h2 className="cd-tutor-title">About the Tutor</h2>
           <div className="cd-tutor-profile">
-            <div className="cd-tutorImgContainer">
+            <div className="tutor--image__container">
               <img
+                className="tutor--image"
                 src={course.tutor.image}
                 alt="tutor"
-                className="cd-tutorImage"
               />
             </div>
             <div className="cd-name-courses">
               <span className="cd-tutor-name">{course.tutor.name}</span>
-              <span className="course-no">62 Courses</span>
+              <span className="course-no">
+                {course.tutorCoursesCount} Courses
+              </span>
             </div>
           </div>
-          <p className="cd-about-tutor">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. At nibh
-            quam odio sit vestibulum sagittis urna.
-          </p>
+          <p className="cd-about-tutor">{course.description}</p>
         </div>
 
         <div className="cd-time-container">
           <div className="ratings-container">
             <p className="cd-ratings">Ratings</p>
-            <hr />
-            <div className="rating-body">
-              <p className="rating-heading">Awesome Tutor</p>
-              <p>
-                excellent tutor does good very very well at all times great one{" "}
-              </p>
-            </div>
-            <hr />
-            <div className="rating-body">
-              <p className="rating-heading">Awesome Tutor</p>
-              <p>
-                excellent tutor does good very very well at all times great one
-              </p>
-            </div>
+            {ratings.length > 0 ? (
+              ratings.map((rating) => (
+                <>
+                  <hr />
+                  <div className="rating-body">
+                    <p className="rating-heading">Awesome Tutor</p>
+                    <p>{rating.description}</p>
+                  </div>
+                </>
+              ))
+            ) : (
+              <>
+                <hr />
+                <div className="rating-body">
+                  <p>This course has no rating yet </p>
+                </div>
+              </>
+            )}
           </div>
         </div>
         <div className="cd-buttons">
@@ -119,10 +128,10 @@ const CourseDetail = () => {
             Pay Now
           </button>
           <MakePayment
-            course={course}
+            course={course!}
             openModal={modalOpen}
             closeModal={() => setModalOpen(false)}
-            email={user?.email}
+            email={user.email}
           />
         </div>
       </div>
@@ -131,5 +140,3 @@ const CourseDetail = () => {
 };
 
 export default CourseDetail;
-
-
