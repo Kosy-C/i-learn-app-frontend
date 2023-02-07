1f5de1900cef0f2b2584dba3647501230cf21d0a
Victor-Olufade <olufadevictor97@gmail>
2023-02-07 15:07:37 +0100

    make navbar coursedetails updateprofile responsive

--- src/pages/courseDetails/courseDetail.tsx
+++ src/pages/courseDetails/courseDetail.tsx
@@ -1,21 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./courseDetail.css";
 import Rating from "../../components/Rating/Rating";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { apiGet } from "../../utils/api/axios";
 import { CourseModel } from "./interface";
-
 import MakePayment from "../../components/Payments/MakePayment";
 import { useAuth } from "../../useContext";
 import LoaderRings from "../../components/Loader/LoaderRings";
-
 const CourseDetail = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [course, setCourse] = useState<CourseModel>();
-
   const { user, loggedInUser, loading } = useAuth();
-
-  // const [email, setEmail] = useState(user?.email);
+  const [email, setEmail] = useState(user?.email);    
   const params = useParams();
   useEffect(() => {
     const getCourseDetail = async () => {
@@ -31,84 +27,101 @@ const CourseDetail = () => {
   const goBack = () => {
     navigate(-1);
   };
-
   const makePayment = () => {
     setModalOpen(true);
   };
-
-  return !user || !course ? (
+  return user == null || course == null ? (
     <LoaderRings />
   ) : (
     <>
+      
       <button className="cd-rate_course_arrowButton" onClick={goBack}>
+        
         &#8249; Go Back
       </button>
-
       <div className="heading-border">
+        
         <h1 className="cd-heading"> About the course</h1>
       </div>
       <div className="cd-container">
+        
         <div
           className="cd-course-container"
           style={{
-            backgroundImage: `url(
-            ${course.course_image}
-          )`,
+            backgroundImage: `url(${course.course_image})`,
+            
           }}
         >
+          
           <h2 className="cd-title">{course.title}</h2>
           <p className="cd-p">{course.description}</p>
           <div className="cd-rating">
+            
             <Rating rating={Number(course.rating)} image={""} color={""} />
           </div>
           <p>
-            Updated{" "}
+            
+            Updated
             {course && new Date(course.createdAt).toLocaleString("en-NG")}
           </p>
-
           <p>{`₦${Number(course.pricing).toLocaleString()}`}</p>
         </div>
-
         <div className="cd-tutor-container">
+          
           <p className="cd-tutor-title">About the Tutor</p>
           <div className="cd-tutor-profile">
-            <img src={course.tutor.image} alt="tutor" />
+            
+            <div className="cd-tutorImgContainer">
+              
+              <img
+                src={course.tutor.image}
+                alt="tutor"
+                className="cd-tutorImage"
+              />
+            </div>
             <div className="cd-name-courses">
+              
               <span className="cd-tutor-name">{course.tutor.name}</span>
               <span className="course-no">62 Courses</span>
             </div>
           </div>
           <p className="cd-about-tutor">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. At nibh
-            quam odio sit vestibulum sagittis urna.
+            {course.tutor.about ? course.tutor.about : null}
+            
           </p>
         </div>
-
         <div className="cd-time-container">
+          
           <div className="ratings-container">
-            <p className="cd-ratings">Ratings</p>
-            <hr />
+            
+            <p className="cd-ratings">Ratings</p> <hr />
             <div className="rating-body">
+              
               <p className="rating-heading">Awesome Tutor</p>
               <p>
-                excellent tutor does good very very well at all times great one{" "}
+                
+              One of the best tutors online. The quality of content and delivery methodology are impeccable.
               </p>
             </div>
             <hr />
             <div className="rating-body">
+              
               <p className="rating-heading">Awesome Tutor</p>
               <p>
-                excellent tutor does good very very well at all times great one
+                
+                Excellent tutor does good very very well at all times great one
               </p>
             </div>
           </div>
         </div>
         <div className="cd-buttons">
+          
           <button className="cal-button" onClick={makePayment}>
+            
             Pay Now
           </button>
           <MakePayment
-            course={course!}
+            course={course}
             openModal={modalOpen}
             closeModal={() => setModalOpen(false)}
             email={user?.email}
@@ -118,5 +131,4 @@ const CourseDetail = () => {
     </>
   );
 };
-
 export default CourseDetail;
